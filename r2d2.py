800b8f3df9aa0ef08daee04929f8cb977f74142a
Caroline Vitória <carolmelo007@hotmail.com>
2026-03-27 17:30:43 -0400

    teste módulo schedule

--- r2d2.py
+++ r2d2.py
@@ -2,9 +2,12 @@ print("testando")
 from plyer import notification
 import pandas as pd
 from datetime import datetime, date
+import schedule
+import time
 
 
-compromissos_hoje = 0
+qtd_compromissos_hoje = 0
+RELATORIO_ENVIADO = True
 
 notification.notify(
     title="Lembrete",
@@ -17,12 +20,32 @@ df = pd.read_csv("Agenda.csv")
 print(date.today())
 
 for index, row in df.iterrows():
+    hoje = datetime.today().strftime('%Y-%M-%D')
     titulo = row["Titulo"]
     data = row["Data"]
     if data == date.today():
         print(f"{titulo} é hoje!")
-        compromissos_hoje+=1
+        qtd_compromissos_hoje+=1
+
         
     print(data)
+def enviaRelatorio():
+    print("Envia Relatório")
 
+def criaRelatorio():
+    print("relatório criado")
+    enviaRelatorio()
+
+def relatorioDiario():
+    if not RELATORIO_ENVIADO:
+        criaRelatorio()
+    else:
+        print("aaaa")
+        
+
+while True:
+    time.sleep(12)
+    schedule.every().day.at("17:29").do(criaRelatorio)
+
+    relatorioDiario()
 
